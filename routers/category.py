759a36506f99e4338e12e39331c6ce87bb0b9c8a
lamakais04-dot <lama.kais04@gmail.com>
2026-01-15 09:17:12 +0200

    added catgory column

--- routers/category.py
+++ routers/category.py
@@ -1,21 +1,34 @@
-from fastapi import APIRouter,HTTPException
-from fastapi.responses import JSONResponse
-from sqlmodel import Session,select
+from fastapi import APIRouter, HTTPException, Depends
+from sqlmodel import Session, select
+from auth_helper import get_user
 from db import engine
 from models.category import Category
+from models.lesting import Listing
+from schemas.lesting import listing
 from schemas.category import category
 
 router = APIRouter()
+
 
 @router.get("/")
 def getCategories():
     with Session(engine) as session:
         statement = select(Category)
         response = session.exec(statement).all()
-        return response    
+        return response
+
+
+@router.get("/{category_id}")
+def get_category_name(category_id: int):
+    with Session(engine) as session:
+        category = session.get(Category, category_id)
+        if not category:
+            raise HTTPException(status_code=404, detail="This category does not exist")
+        return category
+
 
 @router.post("/")
-def createCategory(category_req: category):
+def createCategory(category_req: category, userId: int = Depends(get_user)):
     with Session(engine) as session:
         category_dict = category_req.model_dump()
         category = Category(**category_dict)
@@ -23,3 +36,17 @@ def createCategory(category_req: category):
         session.commit()
         session.refresh(category)
         return category
+
+
+@router.get("/my/categories")
+def get_my_categories(userId=Depends(get_user)):
+    with Session(engine) as session:
+        stmt = (
+            select(Category.id, Category.name)
+            .join(Listing, Listing.categoryid == Category.id)
+            .where(Listing.userid == userId)
+            .distinct()
+        )
+        categories = session.exec(stmt).all()
+
+        return [{"id": c.id, "name": c.name} for c in categories]
